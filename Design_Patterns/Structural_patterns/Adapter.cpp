7daa207d8d619bd3a70d5586873cf6afbb3e0169
AlvinGabrielyan <alvin.gabrielyan2004@gmail.com>
2024-12-05 01:02:43 +0400

    Update Adapter.cpp

--- Design_Patterns/Structural_patterns/Adapter.cpp
+++ Design_Patterns/Structural_patterns/Adapter.cpp
@@ -1,1 +1,40 @@
+#include <iostream>
+using namespace std;
+
+class LegacyRectangle {
+public:
+    void setCoordinates(int x1, int y1, int x2, int y2) {
+        cout << "Setting coordinates: (" << x1 << ", " << y1 << ") to (" << x2 << ", " << y2 << ")\n";
+    }
+};
+
+class Rectangle {
+public:
+    virtual void setDimensions(int width, int height) = 0;
+    virtual ~Rectangle() = default;
+};
+
+class RectangleAdapter : public Rectangle {
+private:
+    LegacyRectangle* legacyRectangle;
+public:
+    RectangleAdapter(LegacyRectangle* legacy) : legacyRectangle(legacy) {}
+
+    void setDimensions(int width, int height) override {
+        int x1 = 0, y1 = 0; 
+        int x2 = x1 + width;
+        int y2 = y1 + height;
+        legacyRectangle->setCoordinates(x1, y1, x2, y2);
+    }
+};
+
+int main() {
+    LegacyRectangle legacyRectangle;
+    Rectangle* rectangle = new RectangleAdapter(&legacyRectangle);
+
+    rectangle->setDimensions(5, 10);
+
+    delete rectangle;
+    return 0;
+}
 
