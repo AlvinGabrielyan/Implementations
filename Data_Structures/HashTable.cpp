752a38b7a8982607b5712beebd38e8db0f65ea3b
AlvinGabrielyan <alvin.gabrielyan2004@gmail.com>
2025-01-07 15:54:45 +0400

    Update HashTable.cpp

--- Data_Structures/HashTable.cpp
+++ Data_Structures/HashTable.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <mutex> 
 
 using namespace std;
 
@@ -13,29 +14,32 @@ struct Node {
 
 class HashTable {
 private:
-    vector<Node*> table; 
-    int capacity;   
-    int size;            
+    vector<Node*> table;
+    int capacity;
+    int size;
+    mutable mutex mtx; 
+
     int hashFunction(int key) const {
         return key % capacity;
     }
 
 public:
-  HashTable(int capacity);
-  ~HashTable();
-  void insert(int key, const string& value);
-  string search(int key) const;
-  void remove(int key);
-  void print() const;
-  int getSize() const;
-  bool isEmpty() const;
+    HashTable(int capacity);
+    ~HashTable();
+    void insert(int key, const string& value);
+    string search(int key) const;
+    void remove(int key);
+    void print() const;
+    int getSize() const;
+    bool isEmpty() const;
 };
 
 HashTable::HashTable(int capacity) : capacity(capacity), size(0) {
-    table.resize(capacity, nullptr); 
+    table.resize(capacity, nullptr);
 }
 
 HashTable::~HashTable() {
+    lock_guard<mutex> lock(mtx); 
     for (int i = 0; i < capacity; ++i) {
         Node* current = table[i];
         while (current != nullptr) {
@@ -47,6 +51,7 @@ HashTable::~HashTable() {
 }
 
 void HashTable::insert(int key, const string& value) {
+    lock_guard<mutex> lock(mtx);
     int index = hashFunction(key);
     Node* newNode = new Node(key, value);
     Node* current = table[index];
@@ -54,45 +59,46 @@ void HashTable::insert(int key, const string& value) {
     if (current == nullptr) {
         table[index] = newNode;
     } else {
-             while (current != nullptr) 
-      {
-        if (current->key == key) {
-          current->value = value;
-          delete newNode;
-          return;
-        }
-        if (current->next == nullptr) break;
-
-        current = current->next;
+        while (current != nullptr) {
+            if (current->key == key) {
+                current->value = value;
+                delete newNode;
+                return;
             }
-         current->next = newNode;
+            if (current->next == nullptr) break;
+            current = current->next;
+        }
+        current->next = newNode;
     }
-    ++size;  
+    ++size;
 }
+
 string HashTable::search(int key) const {
+    lock_guard<mutex> lock(mtx);
     int index = hashFunction(key);
     Node* current = table[index];
 
-     while (current != nullptr) {
+    while (current != nullptr) {
         if (current->key == key) {
-            return current->value; 
-    }
+            return current->value;
+        }
         current = current->next;
     }
     return "The key you are looking for:";
 }
 
 void HashTable::remove(int key) {
+    lock_guard<mutex> lock(mtx); 
     int index = hashFunction(key);
     Node* current = table[index];
     Node* prev = nullptr;
 
-     while (current != nullptr) {
+    while (current != nullptr) {
         if (current->key == key) {
             if (prev == nullptr) {
                 table[index] = current->next;
             } else {
-                prev->next = current->next;    
+                prev->next = current->next;
             }
             delete current;
             --size;
@@ -105,6 +111,7 @@ void HashTable::remove(int key) {
 }
 
 void HashTable::print() const {
+    lock_guard<mutex> lock(mtx); 
     for (int i = 0; i < capacity; ++i) {
         cout << "Bucket " << i << ": ";
         Node* current = table[i];
@@ -117,10 +124,12 @@ void HashTable::print() const {
 }
 
 int HashTable::getSize() const {
+    lock_guard<mutex> lock(mtx);
     return size;
 }
 
 bool HashTable::isEmpty() const {
+    lock_guard<mutex> lock(mtx); 
     return size == 0;
 }
 
@@ -130,12 +139,12 @@ int main() {
     ht.insert(2, "Banana");
     ht.insert(3, "Cherry");
     ht.insert(8, "Watermelon");
-  ht.insert(15, "Bluberry"); 
-    cout << "The contents of the hash table :" << endl;
+    ht.insert(15, "Blueberry");
+    cout << "The contents of the hash table:" << endl;
     ht.print();
 
     cout << "Search key 3: " << ht.search(3) << endl;
-    cout << "Search 10: " << ht.search(10) << endl;
+    cout << "Search key 10: " << ht.search(10) << endl;
 
     cout << "Remove key 3..." << endl;
     ht.remove(3);
